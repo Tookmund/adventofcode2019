1dba89d8409048992ae85ae24041554bf899f763
Jacob Adams <jacob@tookmund.com>
2021-07-10 17:17:25 -0400

    day 1 tests

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,16 +5,38 @@ fn main() -> io::Result<()> {
     let mut total: i32 = 0;
     for line in io::stdin().lock().lines() {
         let massint = line?.parse::<i32>().unwrap();
-        let mut newfuel = getfuel(massint);
-        while newfuel >= 0 {
-            total += newfuel;
-            newfuel = getfuel(newfuel);
-        }
+        total += rocketfuel(massint);
     }
     println!("Total Fuel Required: {}", total);
     Ok(())
 }
 
+// Fuel itself requires fuel because it too has weight
+fn rocketfuel(mass: i32) -> i32 {
+    let mut result = 0;
+    let mut newfuel = getfuel(mass);
+    while newfuel >= 0 {
+        result += newfuel;
+        newfuel = getfuel(newfuel);
+    }
+    result
+}
+
 fn getfuel(mass: i32) -> i32 {
     (mass / 3) - 2
+}
+
+#[test]
+fn part1() {
+    assert_eq!(getfuel(12), 2);
+    assert_eq!(getfuel(14), 2);
+    assert_eq!(getfuel(1969), 654);
+    assert_eq!(getfuel(100756), 33583);
+}
+
+#[test]
+fn part2() {
+    assert_eq!(rocketfuel(14), 2);
+    assert_eq!(rocketfuel(1969), 966);
+    assert_eq!(rocketfuel(100756), 50346);
 }
