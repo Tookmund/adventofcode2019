3cf8a0aabbfbe6125740ca0343e06aadd536e006
Jacob Adams <jacob@tookmund.com>
2021-07-10 20:15:16 -0400

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,44 @@
+use std::env;
 use std::io;
 use std::io::prelude::*;
 
 type Opcode = usize;
 
+const PART2: Opcode = 19690720;
+// vector is length 157, so can't be larger than 156
+const MAXSIZE: Opcode = 156;
+
 fn main() {
     let line = io::stdin().lock().lines().next().unwrap().unwrap();
     let mut prog: Vec<Opcode> = line.split(",").map(|s| s.parse().unwrap()).collect();
     // Replace to restore the values of the running program
-    prog[1] = 12;
-    prog[2] = 2;
+    // 12 and 2 for part 1
+    // No arguments for part 2
+    let count = env::args().count();
+    if count == 1 {
+        for one in 0..MAXSIZE {
+            for two in 0..MAXSIZE {
+                prog[1] = one;
+                prog[2] = two;
+                let result = runprogram(&prog);
+                if result[0] == PART2 {
+                    println!("{} and {} = {}", prog[1], prog[2], PART2);
+                    println!("100 * noun ({}) + verb ({}) = {}", prog[1], prog[2], 100 * prog[1] + prog[2]);
+                    return;
+                }
+            }
+        }
+    }
+    else if count < 3 {
+        let name = env::args().next().unwrap();
+        println!("{} <one> <two> \nor just\n{}\nto find arguments matching {}", name, name, PART2);
+        return;
+    }
+    let mut args = env::args();
+    args.next();
+    for i in 1..3 {
+        prog[i] = args.next().unwrap().parse().unwrap();
+    }
     let result = runprogram(&prog);
     println!("{}", result[0]);
 }
