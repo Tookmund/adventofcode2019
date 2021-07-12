7fb7980e9de57751fa4252ec89148f605e346f33
Jacob Adams <jacob@tookmund.com>
2021-07-12 18:50:40 -0400

    day9: reorganize main to ensure we're using the same stdin object

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,12 +1,15 @@
 mod intcode;
+use intcode::*;
 
 use std::io;
-use intcode::*;
+use std::io::prelude::*;
 
 
 fn main() {
     let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Unable to read program from stdin!");
+    let stdin = io::stdin();
+    let mut input = stdin.lock();
+    input.read_line(&mut line).expect("Unable to read program from stdin!");
     let mut prog: IntCode = line.split(",").map(|s| s.trim().parse::<Opcode>().unwrap()).collect();
-    prog.run(&mut io::stdin().lock(), &mut io::stdout());
+    prog.run(&mut input, &mut io::stdout());
 }
