714e9bbf1da9a5415f08d013f03b27e76dde661e
Jacob Adams <jacob@tookmund.com>
2021-07-12 15:45:57 -0400

    initial day 9 implementation

---
+++ day9/src/main.rs
@@ -0,0 +1,12 @@
+mod intcode;
+
+use std::io;
+use intcode::*;
+
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Unable to read program from stdin!");
+    let mut prog: IntCode = line.split(",").map(|s| s.trim().parse::<Opcode>().unwrap()).collect();
+    prog.run();
+}
