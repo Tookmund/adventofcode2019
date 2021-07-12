d74a7ac69aac3aa66d2a25f489967917dc819b65
Jacob Adams <jacob@tookmund.com>
2021-07-12 17:56:51 -0400

    day9: Add day9 tests

---
+++ day9/tests/day9.rs
@@ -0,0 +1,17 @@
+mod common;
+use common::*;
+
+#[test]
+fn day9_quine() {
+    testprogram_io(vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], None, None, Some(b"109\n1\n204\n-1\n1001\n100\n1\n100\n1008\n100\n16\n101\n1006\n101\n0\n99\n"));
+}
+
+#[test]
+fn day9_16digit() {
+    testprogram_io(vec![1102,34915192,34915192,7,4,7,99,0], None, None, Some(b"1219070632396864\n"));
+}
+
+#[test]
+fn day9_large_number() {
+    testprogram_io(vec![104,1125899906842624,99], None, None, Some(b"1125899906842624\n"))
+}
