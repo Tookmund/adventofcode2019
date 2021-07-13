26126fb0cf820dfefcc35a02b8412561dcc09d4b
Jacob Adams <jacob@tookmund.com>
2021-07-13 15:24:33 -0400

    intcode: clippy, use single char ,
    Strings are slower
    https://rust-lang.github.io/rust-clippy/master/index.html#single_char_pattern

--- intcode/src/main.rs
+++ intcode/src/main.rs
@@ -10,6 +10,6 @@ fn main() {
     let stdin = io::stdin();
     let mut input = stdin.lock();
     input.read_line(&mut line).expect("Unable to read program from stdin!");
-    let mut prog: IntCode = line.split(",").map(|s| s.trim().parse::<Opcode>().unwrap()).collect();
+    let mut prog: IntCode = line.split(',').map(|s| s.trim().parse::<Opcode>().unwrap()).collect();
     prog.run(&mut input, &mut io::stdout());
 }
